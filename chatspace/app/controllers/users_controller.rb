f001045ff9febcac114ed695215f3c6e5231d8f6
orangina92 <user@USER-no-MacBook-Pro.local>
2019-03-08 18:41:11 +0900

    some-fix

--- chatspace/app/controllers/users_controller.rb
+++ chatspace/app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
 
   def index
     @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+    @users = @users.delete(current_user)
     respond_to do |format|
       format.html
       format.json
