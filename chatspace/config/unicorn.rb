b6dc4600732b22df0b6d569dca0643cca882b8c6
orangina92 <user@USER-no-MacBook-Pro.local>
2019-03-14 21:05:47 +0900

    edit-unicorn.rb

--- chatspace/config/unicorn.rb
+++ chatspace/config/unicorn.rb
@@ -2,11 +2,11 @@ app_path = File.expand_path('../../../', __FILE__)
 
 worker_processes 1
 
-working_directory "#{app_path}/chatspace/current"
-listen "#{app_path}/chatspace/shared/tmp/sockets/unicorn.sock"
-pid "#{app_path}/chatspace/shared/tmp/pids/unicorn.pid"
-stderr_path "#{app_path}/chatspace/shared/log/unicorn.stderr.log"
-stdout_path "#{app_path}/chatspace/shared/log/unicorn.stdout.log"
+working_directory "#{app_path}/current"
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
 listen 3000
 timeout 60
